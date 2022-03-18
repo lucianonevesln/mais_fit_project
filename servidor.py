3a6093b359510bee60f23f281a304ad60251df44
Karol <karolinesantos1296@gmail.com>
2022-03-17 23:14:18 -0300

    exclusao da colune valor da banco de dados, atualização da coluna ativo para tinyint e 0 e 1 para disponibilidade, ajuste do response do endpoint /lista para retornar todos os dados necessários para o front

--- servidor.py
+++ servidor.py
@@ -2,6 +2,7 @@
 from flask          import Flask, request, render_template, jsonify
 from flaskext.mysql import MySQL
 
+
 # Acionando ferramenta de contato com o banco de dados
 mysql = MySQL()
 
@@ -17,6 +18,7 @@ app.config['MYSQL_DATABASE_HOST']     = ''
 # Iniciando a conexao com o banco de dados
 mysql.init_app(app)
 
+
 # Criando rota para renderizar o front-end
 @app.route('/', methods=['GET'])
 def index():
@@ -27,7 +29,8 @@ def index():
 def listar():
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute('select nome from sabores') # retorna uma consulta em banco de dados
+    cursor.execute('select id, nome, descricao, link, ativo from sabores')
+    # cursor.execute('select link from sabores') # retorna uma consulta em banco de dados
     nomes = cursor.fetchall()
     conn.commit()
     teste = {}
@@ -35,6 +38,7 @@ def listar():
     return teste
     cursor.close()
     conn.close()
+
 
 # Scrip que define a localidade onde o servidor sera executado
 # if __name__ == '__main__':
