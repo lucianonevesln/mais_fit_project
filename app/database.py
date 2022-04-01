a16879d07357f47c3d39ec77461660d4302b1880
AnedinodSantos <anedinodsantos@gmail.com>
2022-03-31 23:16:35 -0300

    arquivos corretos

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import text, engine_from_config
+from sqlalchemy import text, engine_from_config, Integer
 from config import config
+import jwt
 
 
 engine = engine_from_config(config, prefix='db.')
@@ -7,9 +8,9 @@ engine = engine_from_config(config, prefix='db.')
 
 def lista_sabores_ativos():
     with engine.connect() as con:
-        statement = text("""SELECT nome, descricao, valor, link, ativo 
+        statement = text("""SELECT nome, descricao, link, ativo 
                             FROM sabores
-                            WHERE ativo = 'SIM'""")
+                            WHERE ativo = 1""")
         rs = con.execute(statement)
         sabores = []
         item = rs.fetchone()
@@ -17,3 +18,57 @@ def lista_sabores_ativos():
             sabores.append(dict(item))
             item = rs.fetchone()
     return sabores
+
+def cadastrar_cliente(dados):
+    nome_completo = dados["nome_completo"]
+    cpf = dados["cpf"]
+    nascimento = dados["nascimento"]
+    genero = dados["genero"]
+    celular = dados["celular"]
+    cep = dados["cep"]
+    logradouro = dados["logradouro"]
+    numero = dados["numero"]
+    complemento = dados["complemento"]
+    bairro = dados["bairro"]
+    email = dados["email"]
+    senha = dados["senha"]
+    senha = jwt.encode({"senha":"{0}".format(senha)}, "secret", algorithm="HS256")
+    with engine.connect() as con:
+        statement = text("""INSERT INTO clientes 
+            (   nome_completo, cpf, nascimento, genero, celular, cep, logradouro, numero, 
+                complemento, bairro, email, senha ) values
+            (:nome_completo, :cpf, :nascimento, :genero, :celular, :cep, :logradouro, 
+            :numero, :complemento, :bairro, :email, :senha)
+        """)
+        con.execute(statement, nome_completo=nome_completo, cpf=cpf, 
+                    nascimento=nascimento, genero=genero, celular=celular, cep=cep, 
+                    logradouro=logradouro, numero=numero, 
+                    complemento=complemento, bairro=bairro, email=email, senha=senha)
+
+def listar_clientes():
+    with engine.connect() as con:
+        statement = text("""SELECT nome_completo, cpf, nascimento, genero, celular, cep, logradouro, numero, complemento, bairro, email, senha 
+                            FROM clientes
+                            WHERE ativo = 1""")
+        rs = con.execute(statement)
+        clientes = []
+        item = rs.fetchone()
+        while (item != None):
+            clientes.append(dict(item))
+            item = rs.fetchone()
+    return clientes
+
+
+def cliente_existe(cpf):
+    with engine.connect() as con:
+        statement = text("""SELECT cpf 
+                            FROM clientes
+                            WHERE cpf = :cpf""")
+        rs = con.execute(statement, cpf=cpf)
+        item = rs.fetchone()
+        if item:
+            return "cliente existe"
+
+
+# if __name__ == "__main__":
+#     print(cliente_existe("11111111111"))
--- app/servidor.py
+++ app/servidor.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import *
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def home():
@@ -10,11 +11,23 @@ def home():
 
 @app.route("/lista")
 def listar_sabores():
-    sabores = {}
-    sabores["sabores"] = lista_sabores_ativos()
-    return sabores
+    return jsonify(lista_sabores_ativos())
+
+@app.route("/clientes", methods=['POST'])
+def cadastra_cliente():
+    dados_cliente = request.json
+    cadastrar_cliente(dados_cliente)
+    return {}
+
+@app.route("/clientes", methods=['GET'])
+def lista_cliente():
+    return jsonify(listar_clientes())
+
+@app.route("/cliente-existe/<cpf>")
+def verifica_cliente(cpf):
+    return cliente_existe(cpf)
 
 
-if __name__ == "__main__":
-    app.run("localhost", port=5000, debug=True)
-#     app.run()
+
+# if __name__ == "__main__":
+#     app.run("localhost", port=5000, debug=True)
